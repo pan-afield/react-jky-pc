47fab786822b2d7537e75ce7cd8d253eccdb48c2
‘pan-afield’ <15574847980@163.com>
2022-04-12 17:20:13 +0800

    配置路径别名

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+//路由
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import './App.scss';
+import Login from './pages/Login';
+import Layout from './pages/Layout';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Switch> {/* switch组件用来管理路由模糊匹配的穿透问题，从而实现精准匹配 */}
+          <Route path={'/login'} component={Login}></Route>
+          <Route path={'/'} component={Layout}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/Login/index.js
@@ -0,0 +1,9 @@
+import { Button } from 'antd'
+
+const Login = () => {
+    return (
+        <div><Button type="primary">Button</Button></div>
+    )
+}
+
+export default Login
